647726915d8fb40101e138449c4d89e215aefa28
Aol_0217 <nissi.music@ezweb.ne.jp>
2021-09-15 14:04:01 +0900

    scale

--- Source/Charactor/Enemy_Arrangement.cpp
+++ Source/Charactor/Enemy_Arrangement.cpp
@@ -35,7 +35,7 @@ void csv_load(Block data[][Enemy_Arrangement::CHIP_NUM_X], const char* filename)
 //
 //}
 
-void Enemy_Arrangement::enemy_produce(int csv_file)
+void Enemy_Arrangement::enemy_produce(csv_file_num csv_file)
 {
     // エネミー初期化
     EnemyManager& enemyManager = EnemyManager::Instance();
@@ -61,21 +61,33 @@ void Enemy_Arrangement::enemy_produce(int csv_file)
         {
             // 基準点補正　ステージの左上の座標から始まるように
             constexpr float Reference_point_correction = -249;
+            //float randam_scale = 1;
+
+            srand((unsigned int)time(NULL));
 
             if (Arrangement[y][x].num == Enemy::ENEMYTAG::NORMAL)
             {
+                if ((y < 100 || y > 150) /*&& (x < 100 || x > 150)*/)
+                    if (csv_file == csv_file_num::GAME) randam_scaling(Arrangement[y][x].num);
+
                 NormalEnemy* normalEnemy = new NormalEnemy();
                 normalEnemy->SetPosition(DirectX::XMFLOAT3(Reference_point_correction + x * CHIP_SIZE, 0, Reference_point_correction + y * CHIP_SIZE));
+                normalEnemy->SetScale({ randam_scale, randam_scale, randam_scale });
                 enemyManager.Register(normalEnemy, Enemy::ENEMYTAG::NORMAL);
             }
             if (Arrangement[y][x].num == Enemy::ENEMYTAG::BOMB)
             {       
+                if ((y < 100 || y > 150) /*&& (x < 100 || x > 150)*/)
+                    if (csv_file == csv_file_num::GAME) randam_scaling(Arrangement[y][x].num);
+
                 BombEnemy* bombEnemy = new BombEnemy();
                 bombEnemy->SetPosition(DirectX::XMFLOAT3(Reference_point_correction + x * CHIP_SIZE, 0, Reference_point_correction + y * CHIP_SIZE));
+                bombEnemy->SetScale({ randam_scale, randam_scale, randam_scale });
                 enemyManager.Register(bombEnemy, Enemy::ENEMYTAG::BOMB);
             }
             if (Arrangement[y][x].num == Enemy::ENEMYTAG::RARE)
             {
+
                 RareEnemy* rareEnemy = new RareEnemy();
                 rareEnemy->SetPosition(DirectX::XMFLOAT3(Reference_point_correction + x * CHIP_SIZE, 0, Reference_point_correction + y * CHIP_SIZE));
                 enemyManager.Register(rareEnemy, Enemy::ENEMYTAG::RARE);
@@ -84,4 +96,133 @@ void Enemy_Arrangement::enemy_produce(int csv_file)
     }
 }
 
+void Enemy_Arrangement::randam_scaling(int Arrangement_num)
+{
+    if (Enemy::ENEMYTAG::NORMAL == Arrangement_num)
+    {
+        /*if (normal_randam == true)
+        {
+            randam_num = rand() % 3;
+            normal_randam = false;
 
+            if ((normal_enemy_num[SMALL_SIZE] != normal_enemy_num[MEDIUM_SIZE]) || (normal_enemy_num[SMALL_SIZE] != normal_enemy_num[LARGE_SIZE]))
+            {
+                if (normal_enemy_num[SMALL_SIZE] <= normal_enemy_num[MEDIUM_SIZE])
+                {
+                    if (normal_enemy_num[SMALL_SIZE] < normal_enemy_num[LARGE_SIZE]) randam_num = SMALL_SIZE;
+                    else randam_num = LARGE_SIZE;
+                }
+                else if(normal_enemy_num[MEDIUM_SIZE] <= normal_enemy_num[LARGE_SIZE]) randam_num = MEDIUM_SIZE;
+                else randam_num = LARGE_SIZE;
+            }
+        }
+        else
+        {
+            randam_num = -1;
+            normal_randam = true;
+        }*/
+        
+        if (normal_randam >= 2)
+        {
+            randam_num = -1;
+            normal_randam = 0;
+        }
+        else
+        {
+            randam_num = rand() % 3;
+            if ((normal_enemy_num[SMALL_SIZE] != normal_enemy_num[MEDIUM_SIZE]) || (normal_enemy_num[SMALL_SIZE] != normal_enemy_num[LARGE_SIZE]))
+            {
+                if (normal_enemy_num[SMALL_SIZE] <= normal_enemy_num[MEDIUM_SIZE])
+                {
+                    if (normal_enemy_num[SMALL_SIZE] < normal_enemy_num[LARGE_SIZE]) randam_num = SMALL_SIZE;
+                    else randam_num = LARGE_SIZE;
+                }
+                else if (normal_enemy_num[MEDIUM_SIZE] <= normal_enemy_num[LARGE_SIZE]) randam_num = MEDIUM_SIZE;
+                else randam_num = LARGE_SIZE;
+            }
+            normal_randam++;
+        }
+
+        switch (randam_num)
+        {
+        case SMALL_SIZE:
+            if (normal_enemy_num[SMALL_SIZE] < normal_enemy_limit_num)
+            {
+                randam_scale = 2;
+                normal_enemy_num[SMALL_SIZE]++;
+                 break;
+            }
+        case MEDIUM_SIZE:
+            if (normal_enemy_num[MEDIUM_SIZE] < normal_enemy_limit_num)
+            {
+                randam_scale = 3;
+                normal_enemy_num[MEDIUM_SIZE]++;
+                break;
+            }
+        case LARGE_SIZE:
+            if (normal_enemy_num[LARGE_SIZE] < normal_enemy_limit_num)
+            {
+                randam_scale = 4;
+                normal_enemy_num[LARGE_SIZE]++;
+                break;
+            }
+        default:
+            randam_scale = 1;
+            break;
+        }
+
+    }
+
+    if (Enemy::ENEMYTAG::BOMB == Arrangement_num)
+    {
+        if (bomb_randam == true)
+        {
+            randam_num = rand() % 3;
+            bomb_randam = false;
+        }
+        else
+        {
+            randam_num = -1;
+            bomb_randam = true;
+        }
+
+        /*if (bomb_randam >= 2)
+        {
+            randam_num = rand() % 3;
+            bomb_randam = 0;
+        }
+        else
+        {
+            randam_num = -1;
+            bomb_randam++;
+        }*/
+
+        switch (randam_num)
+        {
+        case SMALL_SIZE:
+            if (bomb_enemy_num[SMALL_SIZE] < bomb_enemy_limit_num)
+            {
+                randam_scale = 2;
+                bomb_enemy_num[SMALL_SIZE]++;
+                break;
+            }
+        case MEDIUM_SIZE:
+            if (bomb_enemy_num[MEDIUM_SIZE] < bomb_enemy_limit_num)
+            {
+                randam_scale = 3;
+                bomb_enemy_num[MEDIUM_SIZE]++;
+                break;
+            }
+        case LARGE_SIZE:
+            if (bomb_enemy_num[LARGE_SIZE] < bomb_enemy_limit_num)
+            {
+                randam_scale = 4;
+                bomb_enemy_num[LARGE_SIZE]++;
+                break;
+            }
+        default:
+            randam_scale = 1;
+            break;
+        }
+    }
+}
--- Source/Charactor/Enemy_Arrangement.h
+++ Source/Charactor/Enemy_Arrangement.h
@@ -9,6 +9,7 @@
 using namespace std;
 #include <string>
 #include <fstream>
+#include <time.h>
 
 class Block
 {
@@ -24,22 +25,32 @@ public:
     static const int CHIP_NUM_X = 250;       // マップの横方向のチップ数
     static const int CHIP_NUM_Y = 250;       // マップの縦方向のチップ数
     static const int CHIP_SIZE = 2;       // %演算子を使用するためint型を使用する
+    static const int NORMAL_ENEMY_NUM = 170;       // normal_enemyの数
+    static const int BOMB_ENEMY_NUM = 108;       // normal_enemyの数
 
 private:
     //------< 変数 >------------------------------------------------------------
 
     Block Arrangement[CHIP_NUM_Y][CHIP_NUM_X];                   // 敵の配置データ
+    int normal_enemy_limit_num = 30;
+    int bomb_enemy_limit_num = 20;
+    int normal_enemy_num[3] = {0};
+    int bomb_enemy_num[3] = {0};
+    int randam_num = 0; 
+
+    float randam_scale = 1;
+
+    /*bool normal_randam = false;
+    bool bomb_randam = false;*/
+
+    int normal_randam = 0;
+    int bomb_randam = 0;
 
 public:
     Enemy_Arrangement() {};
     ~Enemy_Arrangement() {};
 
-    // 更新
-    /*void update();
-
-    void clear();*/
-
-    void enemy_produce(int csv_file = GAME);
+    void randam_scaling(int Arrangement_num);
 
     enum csv_file_num
     {
@@ -48,6 +59,14 @@ public:
         GAME,
     };
 
+    enum enemy_size
+    {
+        SMALL_SIZE,
+        MEDIUM_SIZE,
+        LARGE_SIZE,
+    };
+
+    void enemy_produce(csv_file_num csv_file = GAME);
 };
 
 void csv_load(Block data[][Enemy_Arrangement::CHIP_NUM_X], const char* filename);
